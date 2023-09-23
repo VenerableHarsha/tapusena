5dbf148e2c551c37c35a349eb764a5bdaa84a8c3
VenerableHarsha <harsha.chigurupati@outlook.com>
2023-09-24 01:18:03 +0530

    nope

--- harsha page/script.js
+++ harsha page/script.js
@@ -1,10 +1,16 @@
-// Fetch the filtered data from the server and update the webpage
-fetch('filtered-data.txt')
-    .then(response => response.text())
-    .then(data => {
-        const filteredDataElement = document.getElementById('filtered-data');
-        filteredDataElement.textContent = data;
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
+document.addEventListener('DOMContentLoaded', function() {
+    const fetchDataBtn = document.getElementById('fetchDataBtn');
+    const filteredDataElement = document.getElementById('filtered-data');
+
+    fetchDataBtn.addEventListener('click', function() {
+        // Fetch the filtered data from the Flask route
+        fetch('/filtered-data')
+            .then(response => response.text())
+            .then(data => {
+                filteredDataElement.textContent = data;
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
     });
+});
