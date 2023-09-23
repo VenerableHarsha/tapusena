cfdfe44a78f4c9913b04ab10dedb59397e809e37
kh-ops69 <krishhashia19@gmail.com>
2023-09-24 01:44:51 +0530

    check 2

--- process.py
+++ process.py
@@ -37,14 +37,14 @@ def process_text(text):
     words = text.split()
     return [lemmatize_word(word) for word in words]
 
-df = pd.read_csv("/Users/krishhashia/PycharmProjects/css_mysite/cyberbullying/harras.csv")
+df = pd.read_csv("/Users/krishhashia/PycharmProjects/css_mysite/harras.csv")
 loglikelihood = df.set_index('text')['loglikelihood'].to_dict()
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('', prediction_result='')
+    return render_template('log_in_page/sinup/homepage.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -67,4 +67,4 @@ def npredict(loglikelihood,logprior,tweet="niggers are cotton pickers"):
     return (m,n)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
