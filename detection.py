63fd02ea1e553e9924102486cd61c0b45ecfb00f
kh-ops69 <krishhashia19@gmail.com>
2023-09-24 09:25:18 +0530

    fixed routing

--- detection.py
+++ detection.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 # data = pd.read_csv('login page 2/helpers/help.csv')
 
-@app.route('/')
+@app.route('/blah.html')
 def index():
     return render_template("/blah.html")
 
@@ -91,6 +91,82 @@ def filtered_data():
     ]
 
     return jsonify(rows_to_display)
+
+import pandas as pd
+from nltk.stem import WordNetLemmatizer
+import nltk
+from nltk.corpus import stopwords
+import string
+from nltk import pos_tag
+from nltk.tokenize import word_tokenize
+from flask import Flask, render_template, request
+nltk.download('stopwords')
+l = WordNetLemmatizer()
+stop_words = set(stopwords.words('english'))
+
+def lemmatize_word(word):
+    tokens = word_tokenize(word)
+    tokens = [word for word in tokens if word.lower() not in stop_words]
+    pos_tags = pos_tag(tokens)
+    pos_tag_word = pos_tags[0][1]
+    pos_map = {
+        'N': 'n',  # Noun
+        'V': 'v',  # Verb
+        'R': 'r',  # Adverb
+        'J': 'a'   # Adjective
+    }
+    pos = pos_map.get(pos_tag_word[0], 'n')
+    lemma = l.lemmatize(word, pos=pos)
+    return lemma
+
+def remove_punctuation(line):
+    translator = str.maketrans('', '', string.punctuation)
+    cleaned_line = line.translate(translator)
+    return cleaned_line
+
+def process_text(text):
+    text = str(text)
+    text = text.lower()
+    text = remove_punctuation(text)
+    words = text.split()
+    return [lemmatize_word(word) for word in words]
+
+df = pd.read_csv("/Users/krishhashia/PycharmProjects/css_mysite/harras.csv")
+loglikelihood = df.set_index('text')['loglikelihood'].to_dict()
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def index():
+    return render_template('/homepage.html')
+
+@app.route('/index.html', methods=['GET'])
+def predict():
+    tweet = request.form['tweet']
+    result = npredict(loglikelihood,-2.122046523535116,tweet)
+    return render_template('/index.html', prediction_result=result)
+
+@app.route('/contact.html')
+def contact():
+    return render_template('/contact.html')
+
+@app.route('/aboutus.html')
+def about():
+    return render_template('/aboutus.html')
+
+def npredict(loglikelihood,logprior,tweet="i was beaten up"):
+    word_l = process_text(tweet)
+    p = logprior
+    for word in word_l:
+        if word in loglikelihood:
+            p += loglikelihood[word]
+
+    m = (p / len(tweet)) + 0.71
+    if m>0:
+        n=1
+    else:
+        n=0
+    return (m,n)
 
 if __name__ == '__main__':
     app.run(debug=True)
